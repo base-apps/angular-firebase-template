693e9d391163a8f0925634dff98169b602c12a85
Brian Soumakian <bsoumakian@gmail.com>
2016-09-23 13:23:01 -0700

    update paths

--- karma.conf.js
+++ karma.conf.js
@@ -15,8 +15,8 @@
 
       // list of files / patterns to load in the browser
       files: [
+        'public/js/modernizr.js',
         'public/js/vendor.js',
-        //'node_modules/mockfirebase/browser/mockfirebase.js',
         'public/js/app.js',
         'test/mockfirebase.js',
         'test/karma.boot.js',
